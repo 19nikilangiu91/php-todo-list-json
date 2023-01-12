31ece4c428389c509222a75c466595b8690012ee
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-12 18:44:46 +0100

    Update Template + Data + Methods

--- vue/src/components/jsonTest.vue
+++ vue/src/components/jsonTest.vue
@@ -8,6 +8,27 @@ const Api_url = "http://localhost:8888/api.php";
 
 export default {
   name: "jsonTest",
+  data() {
+
+    return {
+
+      // Inserisco il v-model "newTodo".
+      newTodo: "",
+
+      // Inserisco l'array "todoList".
+      todoList: []
+    };
+  },
+  methods: {
+    // Creo un metodo per il "formSubmit".
+    formSubmit(e) {
+
+      e.preventDefault();
+
+      console.log(this.newTodo);
+
+    }
+  },
   mounted() {
 
     const params = {
@@ -18,7 +39,8 @@ export default {
     axios.get(Api_url, params)
       .then(res => {
         const data = res.data;
-        console.log(data);
+
+        this.todoList = data;
       })
   }
 }
@@ -28,7 +50,17 @@ export default {
 
 <template>
   <div>
-    <h1>Hello World</h1>
+    <h1>TO-DO-LIST</h1>
+    <ul>
+      <!-- Creo un v-for per stamparmi gli elementi contenuti nella "todoListi" -->
+      <li v-for="(todoElement, index) in todoList" :key="index">
+        {{ todoElement.text }}
+      </li>
+    </ul>
+    <form @submit="formSubmit">
+      <input type="text" name="newTodo" placeholder="inserisci elemento" v-model="newTodo">
+      <input type="submit" value="Inserisci">
+    </form>
   </div>
 </template>
 
