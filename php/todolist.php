612553f7219469c7fd2a7ffc763da89b44dfe841
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-12 17:59:34 +0100

    Created todolist.php

---
+++ php/todolist.php
@@ -0,0 +1,55 @@
+<?php
+
+$todoList = [
+    [
+        "text" => "HTML",
+        "completed" => true
+    ],
+    [
+        "text" => "CSS",
+        "completed" => true
+    ],
+    [
+        "text" => "Responsive Design",
+        "completed" => true
+    ],
+    [
+        "text" => "Javascript",
+        "completed" => true
+    ],
+    [
+        "text" => "PHP",
+        "completed" => true
+    ],
+    [
+        "text" => "Laravel",
+        "completed" => false
+    ],
+];
+
+// Dichiaro la variabile "$jsonTodolist" richiamando la funzione "json_encode" che mi codifica il tipo di dato in formato json.
+$jsonTodolist = json_encode($todoList);
+
+// lo inseriamo all'interno del file 'todo.json'.
+$res = file_put_contents('todo.json', $jsonTodolist);
+
+// Dichiaro la variabile "$jsonTodolist" richiamando la funzione "file_get_contents" che mi rilegge il contenuto di un file e ce lo restituisce come stringa.
+$jsonTodolist = file_get_contents('todo.json', true);
+
+// Decodifichiamo il file "$jsonTodolist".
+$todoList = json_decode($jsonTodolist);
+
+
+// Aggiungiamo un elemento all'interno di "$todoList".
+$todoList[] = [
+    "text" => "Prova",
+    "completed" => false
+];
+
+// Rigeneriamo il nostro "$jsonTodolist".
+$jsonTodolist = json_encode($todoList);
+
+// e lo inseriamo all'interno del file 'todo.json'.
+$res = file_put_contents('todo.json', $jsonTodolist);
+
+// var_dump($todoList);
