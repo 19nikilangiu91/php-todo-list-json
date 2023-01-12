35f3e7957208b2fb70931a450335c3913d490dcf
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-12 16:29:56 +0100

    Comunicazione Front-End e Back-End Ok

--- php/api.php
+++ php/api.php
@@ -1,6 +1,13 @@
 <?php
-$testObj = ["Hello world"];
+// Nello svolgere l’esercizio seguite un approccio graduale. Prima assicuratevi che la comunicazione tra "front-end" e "back-end" avvenga correttamente con il passaggio di parametri.
+
+// Aggiungo queste due righe nel php per evitare l'errore di CORS Policy!.
+//---------------------------------- Inserisco l'url della parte "front-end".
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Controll-Allow-Headers: X-Requested-With");
 
 header('Content-Type: application/json');
+
+$testObj = ["Hello world"];
 
 echo json_encode($testObj);
--- vue/src/components/jsonTest.vue
+++ vue/src/components/jsonTest.vue
@@ -1,11 +1,24 @@
 <script>
+// Importo "axios".
+import axios from 'axios';
+
+// Nello svolgere l’esercizio seguite un approccio graduale. Prima assicuratevi che la comunicazione tra "front-end" e "back-end" avvenga correttamente con il passaggio di parametri.
+// Creo una costante per la "Api_url".
+const Api_url = "http://localhost:8888/api.php";
+
 export default {
   name: "jsonTest",
   mounted() {
-    console.log("hello world");
+    axios.get(Api_url)
+      .then(res => {
+        const data = res.data;
+        console.log(data);
+      })
   }
 }
+
 </script>
+
 
 <template>
   <div>
